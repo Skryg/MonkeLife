2c157c015f0f5b909af0ddc7a70bf37e34b260c3
Hub <bernackihubert03@gmail.com>
2023-05-28 21:33:11 +0200

    Player attack system

---
+++ core/src/com/poproject/game/etcs/components/PlayerWeaponComponent.java
@@ -0,0 +1,16 @@
+package com.poproject.game.etcs.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
+
+public class PlayerWeaponComponent implements Component, Pool.Poolable {
+    @Override
+    public void reset() {}
+    public boolean readyToAttack(){
+        return true;
+    }
+    public boolean isProjectile(){
+        return true;
+    }
+
+}
